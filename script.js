24583f42dacc1bd3039e8c41c8dbc2dc0b9172c7
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-05-06 16:46:22 -0300

    adicionando e renderizando cliente

--- script.js
+++ script.js
@@ -23,5 +23,63 @@ const btnConfirmarExclusao = document.getElementById("confirmar-exclusao");
 const btnCancelarExclusao = document.getElementById("cancelar-exclusao");
 
 let clientes = [];
+
 let indexEditando = null;
 let indexExcluindo = null;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //capturo aqui os valores digitados pelo usuário e coloco em variáveis
+  const nome = inputNome.value.trim();
+  const sobrenome = inputSobrenome.value.trim();
+  const cpf = inputCpf.value.trim();
+  const email = inputEmail.value.trim();
+
+  const objCliente = {
+    nome,
+    sobrenome,
+    cpf,
+    email,
+  };
+
+  clientes.push(objCliente);
+
+  form.reset();
+  renderizarTabela();
+});
+
+function renderizarTabela() {
+  tabela.innerHTML = "";
+
+  clientes.forEach((cliente, index) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td>${cliente.nome}</td>
+    <td>${cliente.sobrenome}</td>
+    <td>${cliente.cpf}</td>
+    <td>${cliente.email}</td>
+    `;
+
+    const tdAcoes = document.createElement("td");
+    tdAcoes.classList.add("acoes");
+
+    const btnEditar = document.createElement("button");
+    btnEditar.textContent = "Editar";
+    btnEditar.classList.add("editar");
+
+    const btnExcluir = document.createElement("button");
+    btnExcluir.textContent = "Excluir";
+    btnExcluir.classList.add("excluir");
+
+    tdAcoes.appendChild(btnEditar);
+    tdAcoes.appendChild(btnExcluir);
+
+    tr.appendChild(tdAcoes);
+
+    tabela.appendChild(tr);
+  });
+}
+
+renderizarTabela();
